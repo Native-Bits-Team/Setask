5be040825e5183b22790be4cc8ff3ab9283f6663
Imad Laggoune <imad.laggoune.m@gmail.com>
2024-08-08 11:50:21 +0100

    [HISTORY] [CHANGE]

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//import 'package:window_manager/window_manager.dart'; // [HISTORY]
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +10,23 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: 
+      ThemeData(
+        scaffoldBackgroundColor: Colors.black45,
+        textButtonTheme: const TextButtonThemeData(style: ButtonStyle(
+            backgroundColor: WidgetStatePropertyAll(Colors.amber),
+            padding: WidgetStatePropertyAll(EdgeInsets.all(30.0)),
+            shape: WidgetStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20.0)))),
+            textStyle: WidgetStatePropertyAll(TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+            iconColor: WidgetStatePropertyAll(Colors.white60)))
+        ),
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: TextButton.icon(icon: const Icon(Icons.install_desktop, size: 30,),label: const Text("Install", style: TextStyle(color: Colors.white, fontSize: 30),), onPressed: (){
+            
+          },),
         ),
       ),
     );
